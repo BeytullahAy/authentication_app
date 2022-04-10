6a692c4f102faa37804e7470f6f89f33eaa40787
BeytullahAy <beytullahay47@gmail.com>
2022-04-10 15:55:47 +0300

    home_page güncellendi

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,22 +5,92 @@ import 'package:provider/provider.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () async {
-                Provider.of<Auth>(context, listen: false).signOut();
-                print("logout tıklandı");
-              },
-              icon: Icon(Icons.logout))
-        ],
-      ),
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blueAccent, actions: [
+        IconButton(
+            onPressed: () async {
+              Provider.of<Auth>(context, listen: false).signOut();
+              print("logout tıklandı");
+            },
+            icon: Icon(Icons.logout))
+      ]),
       body: Center(
-        child: Container(
-          child: Text("HOME PAGE"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.blueAccent,
+              backgroundImage: AssetImage('assets/profil.png'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Home Page',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(250, 50),
+                primary: Colors.blueAccent, // background
+                onPrimary: Colors.white, // foreground
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onPressed: () {},
+              child: Text('SİPARİŞLERİM'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(250, 50),
+                primary: Colors.blueAccent, // background
+                onPrimary: Colors.white, // foreground
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onPressed: () {},
+              child: Text('FAVORİLERİM'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(250, 50),
+                primary: Colors.blueAccent, // background
+                onPrimary: Colors.white, // foreground
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onPressed: () {},
+              child: Text('HESAP BİLGİLERİM'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(250, 50),
+                primary: Colors.blueAccent, // background
+                onPrimary: Colors.white, // foreground
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              onPressed: () {},
+              child: Text('YARDIM'),
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
